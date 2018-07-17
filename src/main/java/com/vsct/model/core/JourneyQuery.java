530745f15644ab3ff8341415e3011956605e65e7
Guillaume Leone <gleone.prestataire@oui.sncf>
2018-07-17 17:25:37 +0200

    msg

---
+++ src/main/java/com/vsct/model/core/JourneyQuery.java
@@ -0,0 +1,11 @@
+package com.vsct.model.core;
+
+public class JourneyQuery {
+
+    // Retourne n Journey
+    public final int size;
+
+    public JourneyQuery(int size) {
+        this.size = size;
+    }
+}
---
+++ src/main/java/com/vsct/model/partner/ConnectorResponse.java
@@ -0,0 +1,29 @@
+package com.vsct.model.partner;
+
+import com.vsct.model.core.JourneyQuery;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
+
+/**
+ * Construit une liste de Response.
+ */
+public class ConnectorResponse {
+
+    public final List<PartnerResponse> items;
+
+    public ConnectorResponse(JourneyQuery query) {
+        this.items = buildResponse(query);
+    }
+
+    public List<PartnerResponse> buildResponse(JourneyQuery query) {
+        return Stream.iterate(0, i -> i)
+                .limit(query.size)
+                .map(i -> new PartnerResponse(i, "Value" + i))
+                .collect(toList());
+    }
+}
+
