1635afc0af6ce1f39bace33171c977f841b22419
Guillaume Leone <gleone.prestataire@oui.sncf>
2018-07-18 10:02:02 +0200

    msg

--- src/main/java/com/vsct/model/partner/ConnectorResponse.java
+++ src/main/java/com/vsct/model/partner/ConnectorResponse.java
@@ -3,7 +3,6 @@ package com.vsct.model.partner;
 import com.vsct.model.core.JourneyQuery;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.*;
